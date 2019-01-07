28336b30a855808ec0d5c457c88d0b35dd540452
imaishiuta <shuta25le0n@gmail.com>
2019-01-07 20:02:44 +0900

    edit carrierwave.rb
    add end

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,7 +14,7 @@ CarrierWave.configure do |config|
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
     region: 'ap-northeast-1'
   }
-
+end
   config.fog_directory  = 'upload-test-imai'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/upload-test-imai'
 end
