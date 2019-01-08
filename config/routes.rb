56b2258e1663c73b22d361d12012f2a2f1a3f34b
Taiki0806 <ty4086@iCloud.com>
2019-01-08 12:26:34 +0900

    Edit-routes.rb
    ルーティングの編集。
    ネストを外した。

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "products#index"
-  resources :users do
-    resources :products
-  end
+  resources :users
+  resources :products
   get "productshow" => "products#productshow"
   get "buyproduct" => "products#buyproduct"
   get "userlogout" => "users#userlogout"
