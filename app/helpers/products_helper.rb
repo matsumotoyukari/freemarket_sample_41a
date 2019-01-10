cab18ef2fb4abedd5e7c78a95d5a534ee3dda03e
ALL5B <kugen.1018@gmail.com>
2019-01-10 15:00:43 +0900

    商品の状態、発送日のhelperメソッド作成

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,4 +2,30 @@ module ProductsHelper
   def converting_to_jpy(price)
     "¥" + "#{price.to_s(:delimited, delimiter: ',')}円"
   end
+
+    def converting_to_condition(condition)
+      if condition == 1
+        return "新品、未使用"
+      elsif condition == 2
+        return "未使用に近い"
+      elsif condition == 3
+        return "目立った傷や汚れなし"
+      elsif condition == 4
+        return "やや傷や汚れあり"
+      elsif condition == 5
+        return "傷や汚れあり"
+      elsif condition == 6
+        return "全体的に状態が悪い"
+      end
+    end
+
+    def converting_to_condition(shipmentday)
+      if shipmentday == 1
+        return "1日~2日で発送"
+      elsif shipmentday == 2
+        return "2日~3日で発送"
+      elsif shipmentday == 3
+        return "4日~7日で発送"
+      end
+    end
 end
