022381868d14bd509528d48468faa78249825c77
ALL5B <kugen.1018@gmail.com>
2019-01-10 17:24:20 +0900

    Enumとビューの記述修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,7 +6,20 @@ class Product < ApplicationRecord
   belongs_to :size
   accepts_nested_attributes_for :product_images
 
-  enum condition:["商品状態が入力されていません","新品、未使用","未使用に近い","目立った傷や汚れなし","やや傷や汚れあり","傷や汚れあり","全体的に状態が悪い"]
-  enum shipmentday:["配送日時が入力されていません","1日~2日で発送","2日~3日で発送","4日~7日で発送"]
+  enum condition: {
+    notselect: 0,
+    unused: 1,
+    close_to_unused: 2,
+    no_dirt: 3,
+    slightly_dirty: 4,
+    dirt: 5,
+    bad: 6,
+  }
 
+  enum shipmentday: {
+    noselect: 0,
+    oneday_twoday: 1,
+    twoday_threeday: 2,
+    fourday_sevenday: 3
+  }
 end
