97eb143c7e1a06a5dedbe04cf5cba5adedbdda7e
ALL5B <kugen.1018@gmail.com>
2019-01-10 12:34:55 +0900

    各カテゴリーの商品を格納できるかのテスト

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -6,17 +6,41 @@ describe ProductsController do
       get :index
       expect(response).to render_template :index
     end
+
+    it "カテゴリーレディースに属する商品が格納出来るか" do
+      product = create(:product,category_id: 170)
+      get :index
+      expect(assigns(:ladies).length).to eq 1
+    end
+
+    it "カテゴリーメンズに属する商品が格納されるか" do
+      product = create(:product,category_id: 340)
+      get :index
+      expect(assigns(:mens).length).to eq 1
+    end
+
+    it "カテゴリーベビー・キッズに属する商品が格納されるか" do
+      product = create(:product,category_id: 397)
+      get :index
+      expect(assigns(:baby).length).to eq 1
+    end
+
+    it "カテゴリーコスメに属する商品が格納されるか" do
+      product = create(:product,category_id: 533)
+      get :index
+      expect(assigns(:kosume).length).to eq 1
+    end
   end
 
   describe 'GET #show' do
     it "要求されたproductを@productsに格納できるか" do
-      product = create(:product)
+      product = create(:product,category_id: 340)
       get :show, params: { id: product}
       expect(assigns(:product)).to eq product
     end
 
     it "#showのviewを表示" do
-      product = create(:product)
+      product = create(:product,category_id: 340)
       get :show, params: { id: product}
       expect(response).to render_template :show
     end
@@ -24,15 +48,16 @@ describe ProductsController do
 
   describe 'DELETE #destroy' do
     it "商品消去" do
-      product = create(:product)
+      product = create(:product,category_id: 340)
       expect do
         delete :destroy, params: { id: product}
       end.to change(Product, :count).by(-1)
     end
 
     it "商品消去と同時に紐づく商品画像も消去できる" do
-      product = create(:product)
+      product = create(:product,category_id: 340)
       image = create(:product_image,product_id: product.id)
+      binding.pry
       expect do
         delete :destroy, params: { id: product}
       end.to change(ProductImage, :count).by(-1)
