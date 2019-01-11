9774d61e3be854130141587251db6cf1d5e76ebc
imaishiuta <shuta25le0n@gmail.com>
2019-01-11 12:30:20 +0900

    Edit controller
    added ‘protect_from_forgery except: :pay’ because rails reject token
    from outside

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
+  protect_from_forgery except: :pay
 
   def index
+  end
+
+  def register_cregit_card
+  end
+
+  def pay
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    @user = User.find(current_user)
+    @user.cardtoken = params[:cardtoken]
+    @user.save
+    redirect_to root_path
   end
 
 end
