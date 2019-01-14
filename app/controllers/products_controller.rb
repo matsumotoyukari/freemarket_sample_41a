8e73b89defc9a75f981cf7d379d87a98f4c64388
ALL5B <kugen.1018@gmail.com>
2019-01-14 19:46:52 +0900

    リファクタリング

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,18 +1,10 @@
 class ProductsController < ApplicationController
 
   def index
-    @products = Product.all
-    ladies_ids = Category.find(1).subtree_ids
-    @ladies = Product.where(category_id: ladies_ids.first..ladies_ids.last).limit(4).order('created_at DESC')
-    mens_ids = Category.find(2).subtree_ids
-    mens_ids.pop(1)
-    @mens = Product.where(category_id: mens_ids.first..mens_ids.last).limit(4).order('created_at DESC')
-    baby_ids = Category.find(3).subtree_ids
-    baby_ids.pop(1)
-    @baby_products = Product.where(category_id: baby_ids.first..baby_ids.last).limit(4).order('created_at DESC')
-    interior_ids = Category.find(4).subtree_ids
-    interior_ids.pop(1)
-    @interior_products = Product.where(category_id: interior_ids.first..interior_ids.last).limit(4).order('created_at DESC')
+    @ladies = search_product(Category.find(1))
+    @mens = search_product(Category.find(2))
+    @baby_products = search_product(Category.find(3))
+    @interior_products = search_product(Category.find(4))
   end
 
   def new
@@ -44,6 +36,9 @@ class ProductsController < ApplicationController
     @keyword = params[:keyword]
     @products = Product.where('name LIKE(?) OR detail  LIKE(?)',"%#{params[:keyword]}%","%#{params[:keyword]}%").limit(20)
   end
+
+
+
 
   private
   def product_params
