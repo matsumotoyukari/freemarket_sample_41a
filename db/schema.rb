4e12317a98d9c3be0e64d4ba06cb3ab5cd664835
imaishiuta <shuta25le0n@gmail.com>
2019-01-14 16:16:36 +0900

    create migration file and did migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190112113118) do
+ActiveRecord::Schema.define(version: 20190114032915) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -43,6 +43,11 @@ ActiveRecord::Schema.define(version: 20190112113118) do
     t.integer  "sizetype_id"
     t.index ["ancestry"], name: "index_categories_on_ancestry", using: :btree
     t.index ["sizetype_id"], name: "index_categories_on_sizetype_id", using: :btree
+  end
+
+  create_table "mypayjps", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "product_images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -123,6 +128,7 @@ ActiveRecord::Schema.define(version: 20190112113118) do
     t.datetime "updated_at",                                        null: false
     t.text     "name",                   limit: 65535
     t.string   "cardtoken"
+    t.string   "customerid"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
