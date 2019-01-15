3c105a22f6d7217709274bf06ac25fdaf67985c7
imaishiuta <shuta25le0n@gmail.com>
2019-01-15 18:19:32 +0900

    Edit point.js
    メソッドをリファクタリング

--- app/assets/javascripts/point.js
+++ app/assets/javascripts/point.js
@@ -1,26 +1,20 @@
 $(function() {
   function buildHTML() {
-    $(".btn-default").remove();
-    html = `<div class="btn-default btn-red">
+    if(value > price){
+      html = `<div class="btn-default btn-red">
               ポイントが不足しています。
-            </div>
-              `
-    return html
-  }
-  function alertHTML() {
-    $(".btn-default").remove();
-    html = `<div class="btn-default btn-red">
+            </div>`
+    } else if(value > point) {
+      html = `<div class="btn-default btn-red">
               それ以上のポイントは使用できません。
             </div>`
-    return html
-  }
-  function recoveryHTML() {
-    $(".btn-default").remove();
-    html = ` <button class="btn-default btn-red", type="submit">
+    } else if(value <= $(this).data('point') || value <= price) {
+      html = ` <button class="btn-default btn-red", type="submit">
               <div class="buy-botton">
               購入する
               </div>
             </button>`
+    }
     return html
   }
 
@@ -40,17 +34,7 @@ $(function() {
       $(".buy-price-cell-fix").text("0");
       $(".point").text("0")
     }
-    if(value > price) {
-      html = alertHTML()
-      $(".button-wrapper").append(html);
-    }
-    else if(value > point) {
-      html = buildHTML()
-      $(".button-wrapper").append(html);
-    }
-    else if(value <= $(this).data('point') || value <= price) {
-      html = recoveryHTML()
-      $(".button-wrapper").append(html);
+    html = buildHTML()
+    $(".button-wrapper").append(html);
     };
   });
-});
