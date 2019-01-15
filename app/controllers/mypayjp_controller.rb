b048009c70e47af1748b6be9b8c7654b69a0b938
imaishiuta <shuta25le0n@gmail.com>
2019-01-15 17:08:36 +0900

    edit mypayjp_controller.rb
    added transaction

--- app/controllers/mypayjp_controller.rb
+++ app/controllers/mypayjp_controller.rb
@@ -9,23 +9,26 @@ class MypayjpController < ApplicationController
   end
 
   def create_charge
-    @product = Product.find(params[:id])
-    customer_id = current_user.customerid
-    point = params[:price].to_i
-    amount = @product.price - point
-    if current_user.point
-      save_users_point(point, amount)
-    end
-    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
-    PaysHelper.create_charge(amount, customer_id)
-    @trade = Trade.find_by(product_id: params[:id])
-    @trade.deal = true
-    if @trade.save
-      save_sellers_point(@product, amount)
+      @product = Product.find(params[:id])
+      customer_id = current_user.customerid
+      point = params[:price].to_i
+      amount = @product.price - point
+      User.transaction do
+        if current_user.point
+          save_users_point(point, amount)
+        end
+          Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+          PaysHelper.create_charge(amount, customer_id)
+            Trade.transaction do
+            @trade = Trade.find_by(product_id: params[:id])
+            @trade.deal = true
+            @trade.save
+            save_sellers_point(@product, amount)
+          end
+      end
       redirect_to root_path
-    else
-      redirect_to mypayjp_path
-    end
+  rescue
+    redirect_to  product_path
   end
 
 end
