42ede70afa074ee7e2dd232cbe7fec5410aac078
imaishiuta <shuta25le0n@gmail.com>
2019-01-15 18:33:53 +0900

    Edit mypayjp_controller

--- app/controllers/mypayjp_controller.rb
+++ app/controllers/mypayjp_controller.rb
@@ -16,6 +16,7 @@ require 'payjp'
     if current_user.point
       save_users_point(point, amount)
     end
+
     Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     PaysHelper.create_charge(amount, customer_id)
     Trade.transaction do
@@ -24,6 +25,7 @@ require 'payjp'
       @trade.save
       save_sellers_point(@product, amount)
     end
+
     redirect_to root_path
     rescue
     redirect_to  product_path
