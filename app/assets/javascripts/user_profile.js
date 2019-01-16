4e74cfaefc0f35eb9c3680d55692c865f3ec08f0
imaishiuta <shuta25le0n@gmail.com>
2019-01-16 17:29:42 +0900

    delete user_profile.js

--- app/assets/javascripts/user_profile.js
+++ app/assets/javascripts/user_profile.js
@@ -1,8 +1,1 @@
-$(function(){
-    var index = $(".mypage-tabs li").index(this);
-    $(".mypage-item-list li").css('display','none');
-    $('.mypage-item-list li').css('display','block');
-    $(".mypage-tabs li").removeClass('active');
-    $(this).addClass('active');
-  });
-});
+
