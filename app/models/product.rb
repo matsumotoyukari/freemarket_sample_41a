41ea7054b9296c97c72866fdf598b5d323679949
imaishiuta <shuta25le0n@gmail.com>
2019-01-16 13:57:42 +0900

    edit product.rb
    comment out the validations to check product selling function in
    production environment

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,14 +11,14 @@ class Product < ApplicationRecord
     validates :name, length: { maximum: 40 }
     validates :detail, length: { maximum: 1000 }
     validates :category_id
-    validates :condition
-    validates :size_id
-    validates :shipingfee_id
-    validates :shipment_id
-    validates :area_id
-    validates :shipmentday
+    #validates :condition
+    #validates :size_id
+    #validates :shipingfee_id
+    #validates :shipment_id
+    #validates :area_id
+    #validates :shipmentday
     validates :product_images
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+    #validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   end
 
   enum condition: {
