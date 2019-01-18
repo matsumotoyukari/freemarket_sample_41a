a87586d5d0dafd488ae845ffbb0e1b4d4e759652
ALL5B <kugen.1018@gmail.com>
2019-01-18 11:59:49 +0900

    いいねテーブル修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190115085458) do
+ActiveRecord::Schema.define(version: 20190118025036) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20190115085458) do
     t.integer  "product_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["product_id", "user_id"], name: "index_likes_on_product_id_and_user_id", unique: true, using: :btree
     t.index ["product_id"], name: "index_likes_on_product_id", using: :btree
     t.index ["user_id"], name: "index_likes_on_user_id", using: :btree
   end
