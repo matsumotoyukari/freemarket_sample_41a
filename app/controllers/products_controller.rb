75d51494182984f02837500810868ff08d425b27
imaishiuta <shuta25le0n@gmail.com>
2019-01-22 13:26:02 +0900

    Edit products_controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -26,7 +26,7 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
     @seller = User.find_by(id: @product.seller)
-    @another_product = @seller.products.where(seller: @seller.id).where.not(id: @product.id)
+    @another_product = @seller.products.where.not(id: @product.id)
     @comment = Comment.new
     @comments = Comment.where(product_id: @product.id)
   end
